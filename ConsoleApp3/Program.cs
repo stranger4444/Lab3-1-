4924ca99083b9dbc0ee6a9214acbff529ff0a80c
stranger4444 <dronovdenis36@gmail.com>
2023-10-02 10:02:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the length of the first side of the rectangle:");
+        double side1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the length of the other side of the rectangle:");
+        double side2 = double.Parse(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(side1, side2);
+
+        Console.WriteLine($"Area of rectangle: {rectangle.Area}");
+        Console.WriteLine($"Perimeter of rectangle: {rectangle.Perimeter}");
+
+        Console.ReadLine();
+
+     
+    }
+}
+    
